6aa3b0b2e8e8cb1dd1b0867c7035e1af2fa1f316
Ryan Miller <rmill865@gmail.com>
2016-12-22 18:33:44 -0700

    saving progress

--- app/js/signin.js
+++ app/js/signin.js
@@ -4,4 +4,16 @@ window.onload = function() {
   if (name) {
     $('#name').val(name);
   }
+
+  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+    console.log('downloaded');
+    FB.init({
+      appId: '296945143653057',
+      version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+    });
+
+    FB.login(function(response) {
+       console.log(response);
+     }, {scope: 'public_profile'});
+  });
 }
